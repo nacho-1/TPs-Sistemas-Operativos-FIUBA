76bd9c5b6411c05aaa1faeb66ef325609e898e66
Martin <manus@fi.uba.ar>
2023-04-17 01:07:09 -0300

    agrego comando history v0

--- shell/builtin.c
+++ shell/builtin.c
@@ -1,5 +1,6 @@
 #include "builtin.h"
 
+
 char buf[BUFLEN] = { 0 };
 
 // returns true if the 'exit' call
@@ -41,7 +42,7 @@ cd(char *cmd)
 				snprintf(prompt, sizeof prompt, "(%s)", home);
 				return true;
 			}
-		} else if (cmd[2] == ' ') {
+		}else if(cmd[2] == SPACE){
 			int cmd_len = strlen(cmd);
 			char dir[cmd_len];
 			strcpy(dir, cmd + 3);
@@ -91,7 +92,39 @@ pwd(char *cmd)
 int
 history(char *cmd)
 {
-	// Your code here
+	// TODO - add optional argument number of commands to show
+	if (strcmp(cmd, "history") == 0){
+		FILE * fp;
+   		char * line = NULL;
+   		size_t len = 0;
+		char *histfile = getenv("HISTFILE");		
 
-	return 0;
+		if(histfile == NULL){
+			char default_histfile[100] = {'\0'}	;
+			char *home = getenv("HOME");
+			strcat(default_histfile, home);
+			strcat(default_histfile, "/.sisop_history");
+			printf("DEFAULT HISTFILE: %s\n", default_histfile);
+			fp = fopen(default_histfile, "r");
+		}    
+		else{
+			printf("ENV HISTFILE: %s\n", histfile);
+			fp = fopen(histfile, "r");
+		}			
+
+		if (fp == NULL)
+			exit(EXIT_FAILURE);
+
+		while (getline(&line, &len, fp) != -1) {
+			printf("%s", line);
+		}
+
+		fclose(fp);
+		if (line)
+			free(line);
+
+		return true;
+	}
+
+	return false;
 }
