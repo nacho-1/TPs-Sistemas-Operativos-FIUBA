d5f0a206e6fe45c09fd5bdec713f06805dcbbd23
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2023-04-20 23:24:32 -0300

    feat: improve malloc tests

---
+++ malloc/malloc.test.c
@@ -0,0 +1,75 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+#include "testlib.h"
+#include "malloc.h"
+
+static void
+correct_copied_value(void)
+{
+	char *test_string = "FISOP malloc is working!";
+
+	char *var = malloc(100);
+
+	strcpy(var, test_string);
+
+	ASSERT_TRUE("allocated memory should contain the copied value",
+	            strcmp(var, test_string) == 0);
+
+	free(var);
+}
+
+static void
+correct_amount_of_mallocs(void)
+{
+	struct malloc_stats stats;
+
+	char *var = malloc(100);
+
+	free(var);
+
+	get_stats(&stats);
+
+	ASSERT_TRUE("amount of mallocs should be one", stats.mallocs == 1);
+}
+
+static void
+correct_amount_of_frees(void)
+{
+	struct malloc_stats stats;
+
+	char *var = malloc(100);
+
+	free(var);
+
+	get_stats(&stats);
+
+	ASSERT_TRUE("amount of frees should be one", stats.frees == 1);
+}
+
+static void
+correct_amount_of_requested_memory(void)
+{
+	struct malloc_stats stats;
+
+	char *var = malloc(100);
+
+	free(var);
+
+	get_stats(&stats);
+
+	ASSERT_TRUE("amount of requested memory should be 100",
+	            stats.requested_memory == 100);
+}
+
+int
+main(void)
+{
+	run_test(correct_copied_value);
+	run_test(correct_amount_of_mallocs);
+	run_test(correct_amount_of_frees);
+	run_test(correct_amount_of_requested_memory);
+
+	return 0;
+}
