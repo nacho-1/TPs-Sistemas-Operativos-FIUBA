26f4ea00ba66ab4dd484b8a3eac78067645f5c6c
ibertolini <56204692+nacho-1@users.noreply.github.com>
2023-04-23 14:13:28 -0300

    Fix: libero recursos de history en los pipes

--- shell/exec.c
+++ shell/exec.c
@@ -234,6 +234,7 @@ exec_cmd(struct cmd *cmd)
 			exec_cmd(p->leftcmd);
 
 			free_command(cmd);
+			free_history();
 			fflush(stdout);
 			_exit(EXIT_FAILURE);
 		}
@@ -254,6 +255,7 @@ exec_cmd(struct cmd *cmd)
 			exec_cmd(p->rightcmd);
 
 			free_command(cmd);
+			free_history();
 			fflush(stdout);
 			_exit(EXIT_FAILURE);
 		}
@@ -261,6 +263,7 @@ exec_cmd(struct cmd *cmd)
 		close(read_pipe);
 		close(write_pipe);
 		free_command(cmd);
+		free_history();
 
 		int left_status, right_status;
 		if (left_pid != -1)
