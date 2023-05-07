7efa75a420261e73ce62478df20ad1100635f090
feijooso <sfeijoo@fi.uba.ar>
2023-05-06 21:34:34 -0300

    format

--- malloc/malloc.test.c
+++ malloc/malloc.test.c
@@ -10,7 +10,9 @@ successful_malloc_returns_non_null_pointer(void)
 {
 	char *var = malloc(100);
 
-	ASSERT_TRUE("[MALLOC - success]successful malloc returns non null pointer", var != NULL);
+	ASSERT_TRUE(
+	        "[MALLOC - success]successful malloc returns non null pointer",
+	        var != NULL);
 
 	free(var);
 }
@@ -24,7 +26,8 @@ correct_copied_value(void)
 
 	strcpy(var, test_string);
 
-	ASSERT_TRUE("[MALLOC - copied value] allocated memory should contain the copied value",
+	ASSERT_TRUE("[MALLOC - copied value] allocated memory should contain "
+	            "the copied value",
 	            strcmp(var, test_string) == 0);
 
 	free(var);
@@ -41,7 +44,8 @@ correct_amount_of_mallocs(void)
 
 	get_stats(&stats);
 
-	ASSERT_TRUE("[MALLOC - stats] amount of mallocs should be one", stats.mallocs == 1);
+	ASSERT_TRUE("[MALLOC - stats] amount of mallocs should be one",
+	            stats.mallocs == 1);
 }
 
 static void
@@ -55,7 +59,8 @@ correct_amount_of_frees(void)
 
 	get_stats(&stats);
 
-	ASSERT_TRUE("[MALLOC - stats] amount of frees should be one", stats.frees == 1);
+	ASSERT_TRUE("[MALLOC - stats] amount of frees should be one",
+	            stats.frees == 1);
 }
 
 static void
@@ -73,18 +78,22 @@ correct_amount_of_requested_memory(void)
 	            stats.requested_memory == 100);
 }
 
-static void fail_if_malloc_exceeds_memory_limit(void) {
+static void
+fail_if_malloc_exceeds_memory_limit(void)
+{
 	struct malloc_stats stats;
 	size_t SMALL_BLOCK_SIZE = 16384;
 
-	char *var = malloc(SMALL_BLOCK_SIZE + 1); //TODO: AJUSTAR A MAS BLOQUES
+	char *var = malloc(SMALL_BLOCK_SIZE + 1);  // TODO: AJUSTAR A MAS BLOQUES
 	get_stats(&stats);
 
 	ASSERT_TRUE("[MALLOC - limit] should return NULL pointer", var == NULL);
 	free(var);
 }
 
-static void fail_if_requests_negative_amount(void) {
+static void
+fail_if_requests_negative_amount(void)
+{
 	struct malloc_stats stats;
 
 	char *var = malloc(-1);
@@ -94,31 +103,38 @@ static void fail_if_requests_negative_amount(void) {
 	free(var);
 }
 
-static void alloc_min_size_if_requested_less(void) {
+static void
+alloc_min_size_if_requested_less(void)
+{
 	struct malloc_stats stats;
 
 	char *var = malloc(1);
 
 	get_stats(&stats);
-	ASSERT_TRUE("[MALLOC - min] amount of requested memory should be 64 (min size)", var == NULL);
+	ASSERT_TRUE("[MALLOC - min] amount of requested memory should be 64 "
+	            "(min size)",
+	            var == NULL);
 
 	free(var);
 }
 
-static void fail_if_requested_zero(void) {
+static void
+fail_if_requested_zero(void)
+{
 	struct malloc_stats stats;
 
 	char *var = malloc(0);
 	get_stats(&stats);
 
-	ASSERT_TRUE("[MALLOC - requested 0] should return NULL pointer", var == NULL);
+	ASSERT_TRUE("[MALLOC - requested 0] should return NULL pointer",
+	            var == NULL);
 	free(var);
 }
 
 int
 main(void)
 {
-	//TESTS MALLOC
+	// TESTS MALLOC
 	run_test(successful_malloc_returns_non_null_pointer);
 	run_test(correct_copied_value);
 	run_test(correct_amount_of_mallocs);
