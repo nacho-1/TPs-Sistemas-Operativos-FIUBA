4616213bd186ed3c885cf8a336262ed0cafd7917
ibertolini <56204692+nacho-1@users.noreply.github.com>
2023-06-23 14:52:04 -0300

    Empiezo la implementacion de un vsfs
    Defino las macros basicas para definir el filesystem
    Defino una estructura de inodo
    Creo funcion init, mas que nada para debugueo por ahora

--- fisopfs/fisopfs.c
+++ fisopfs/fisopfs.c
@@ -10,6 +10,58 @@
 #include <stdlib.h>
 #include <errno.h>
 
+#define ALIGN4(s) (((((s) -1) >> 2) << 2) + 4)
+#define ALIGNPO2(s) ((s) > 0 ? (1 << (32 - __builtin_clz((s) - 1))) : 1)
+
+#define N_BLOCKS 4096 // arbitrario
+#define BLOCK_SIZE 4096 // basado en el tamaño de las paginas en x86
+
+// Macros de inodos
+#define INODE_SIZE ALIGNPO2(sizeof(inode_t)) // alineado a potencia de 2 para que entren justo en un bloque
+#define N_INODES_PER_BLOCK (BLOCK_SIZE / INODE_SIZE)
+#define N_DATA_BLOCKS_PER_INODE 32 // arbitrario, idealmente que sea potencia de 2
+#define N_INODE_BLOCKS (N_BLOCKS / N_INODES_PER_BLOCK) // al menos un inodo por cada bloque, potencias de 2 para que la division no tenga resto
+#define N_INODES (N_INODE_BLOCKS * N_INODES_PER_BLOCK)
+
+#define N_DATA_BLOCKS (N_BLOCKS - (3 + N_INODE_BLOCKS))
+
+// Bloques importantes
+#define SUPERBLOCK 0
+#define INODE_BITMAP 1
+#define DATA_BITMAP 2
+#define INODE_TABLE 3
+#define DATA_REGION (INODE_TABLE + N_INODE_BLOCKS)
+
+
+static uint8_t blocks[N_BLOCKS * BLOCK_SIZE]; // El filesystem
+
+static struct inode {
+	mode_t mode;
+	uid_t uid;
+	gid_t gid;
+	size_t size;
+	uint8_t n_blocks;
+	unsigned data_blocks[N_DATA_BLOCKS_PER_INODE];
+	time_t atim;
+	time_t mtim;
+	time_t ctim;
+};
+typedef struct inode inode_t;
+static uint8_t *inode_table = &blocks[INODE_TABLE * BLOCK_SIZE]; // shortcut a la tabla de inodos
+
+static void *
+fisopfs_init(struct fuse_conn_info *conn)
+{
+	printf("[debug] fisopfs init\n");
+	printf("[debug] There's %u blocks of %u bytes each\n", N_BLOCKS, BLOCK_SIZE);
+	printf("[debug] Inode size is %lu bytes aligned to %u\n", sizeof(inode_t), INODE_SIZE);
+	printf("[debug] There's %u inodes in %u blocks\n", N_INODES, N_INODE_BLOCKS);
+	printf("[debug] There's %u data blocks\n", N_DATA_BLOCKS);
+	printf("[debug] Data region start block: %u\n", DATA_REGION);
+
+	return NULL;
+}
+
 static int
 fisopfs_getattr(const char *path, struct stat *st)
 {
@@ -83,6 +135,7 @@ fisopfs_read(const char *path,
 }
 
 static struct fuse_operations operations = {
+	.init = fisopfs_init,
 	.getattr = fisopfs_getattr,
 	.readdir = fisopfs_readdir,
 	.read = fisopfs_read,
